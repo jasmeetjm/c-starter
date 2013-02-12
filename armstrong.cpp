d823329575bff1e71c9fd2cbfbdcbe9f9cd04772
jasmeet <jasmeetjm@gmail.com>
2013-02-12 23:37:28 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -1,40 +1,36 @@
+/*to check if given no. is armstrong number or not */
 
 #include<iostream>
 #include<math.h>
 using namespace std;
-bool armstrong(int num)
+bool armstrong(int num) //bool function which return if a given is true of false
 {
-int num1=num;
-int count=0;
-int sum=0;
-while (num1!=0)
-{
-num1=num1/10;
-++count;
-}
-num1=num;
-while (num1!=0)
-{
+  int num1=num,
+      count=0,  //variable to count no. of digits in an integer
+      sum=0;    //variable to calculate te sum of it's powers
+  while (num1!=0) { //to calculate no. of digits
+    num1=num1/10;
+    ++count;
+  }
+  num1=num;   
+  while (num1!=0) {   //to calculate sum of their powers
+    sum+=pow((num1%10),count);
+    num1=num1/10;
+  }
+  if (sum==num)   //if an armstrong number will return true to the main
+    return true;
+  else 
+    return false;
+}  
 
-sum+=pow((num1%10),count);
-num1=num1/10;
-}
-if (sum==num)
-{
-return true;
-}
-else 
-return false;
-}
 int main()
 {
-int num;
-cin>>num;
-if (armstrong(num))
-{
-cout<<"no. is armstrong"<<endl;
-return 0;
-}
-cout<<"number is not armstrong "<<endl;
-return 0;
+  int num;  //variable for an integer 
+  cin>>num; //user input 
+  if (armstrong(num)) { //function calling
+    cout<<"no. is armstrong"<<endl; //if return true
+    return 0;
+  }
+  cout<<"number is not armstrong "<<endl; //if return is false
+  return 0;
 }
