005d8e0e181c85cd5bd9b59680e9689e0268161c
jasmeet <jasmeetjm@gmail.com>
2013-02-13 00:38:26 +0530

    Update file_linecount.c

--- file_linecount.c
+++ file_linecount.c
@@ -1,29 +1,24 @@
+/*to count the number of lines */
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-	FILE *fptr;
-	char filename[]="2.txt";
-	char ch;
-	int count=0;
-	fptr=fopen(filename,"r");
-		
-		if(fptr==NULL)
-{
-	fprintf(stderr,"NOTHING");
-	exit(-1);	
-	
-}
-	while((ch=fgetc(fptr))!=EOF)
-{
-		if(ch==10)
-{
-		count++;
-}
-		
-}	
-	fclose(fptr);
+	FILE *fptr;		//declaring file pointer
+	char filename[]="2.txt";	//declaring filename
+	char ch;		
+	int count=0;		//to count no. of lines
+	fptr=fopen(filename,"r");	//opening file
+	if(fptr==NULL) {		//if file not opened due to any reason
+		fprintf(stderr,"NOTHING");
+		exit(-1);	
+	}
+	while((ch=fgetc(fptr))!=EOF) {	//fetch char by char till eof
+		if(ch=='\n') { //enever it enconter new line
+			count++;
+		}
+	}	
+	fclose(fptr);	//closinga file
 	fprintf(stdout,"%d",count);		
-	
 	return 0;
 }
