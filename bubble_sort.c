23f42ee0443ae8bc098cfdbb3a1fbaf277d2a569
jasmeet <jasmeetjm@gmail.com>
2013-02-12 23:46:29 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,21 +1,22 @@
+/* to sort a given set through bubble sort */
+
 	#include<stdio.h>
-	#define ARRAY_SIZE 10
+	#define ARRAY_SIZE 10	//macros to declare ARRAY_SIZE is a representation of 10
 	
-	int array[ARRAY_SIZE]={12,23,32,4,23,1,45,213,45,223};
-int *bubble_sort(int *arr)
+	int array[ARRAY_SIZE]={12,23,32,4,23,1,45,213,45,223};	//any given set of array
+int *bubble_sort(int *arr)	//function to sort numbers
 {
-		int 	i,
-			j;
-		int count=1;
-		int swap=1;	
-		for(i=0;i<ARRAY_SIZE-1 && swap;i++) {
-			swap=0;
+		int 	i,	//variables
+			j;	
+		int swap=1;	//act as flag	
+		for(i=0;i<ARRAY_SIZE-1 && swap;i++) {	//if flag is true only then it will work
+			swap=0;				//flag is unset so that it again has to satisfy to come in loop	
 			for(j=0;j<ARRAY_SIZE-1-i;j++) {
-				if(arr[j]>arr[j+1]) {
+				if(arr[j]>arr[j+1]) {		//to swap 2 no's
 					arr[j]=arr[j]^arr[j+1];
 					arr[j+1]=arr[j]^arr[j+1];
 					arr[j]=arr[j]^arr[j+1];
-					swap=1;
+					swap=1;		//if no.'s are swapped only then it works in loop
 				}
 			}
 		}
@@ -24,7 +25,7 @@ int *bubble_sort(int *arr)
 }
 int main()
 {
-	bubble_sort(array);
+	bubble_sort(array);	
 	int i=0;
 	while(i<ARRAY_SIZE) {
 		printf("%d\n",array[i]);
