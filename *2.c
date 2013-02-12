19c7c97d143a10e38edf4229a3d9de97e8ab3218
jasmeet <jasmeetjm@gmail.com>
2013-02-12 22:36:43 +0530

    Update *2.c

--- *2.c
+++ *2.c
@@ -1,34 +1,28 @@
+/* star combination program*/
+
 #include<stdio.h>
 int main()
 {
-int i=0,j=0;
-int line=1;
-
-int ui ;
- printf("enter lines");
- scanf("%d",&ui);
-	
-	while(line<=ui)
-{
+	int i=0,
+	j=0,
+	line=1,
+	ui ;
+	printf("enter lines");
+	scanf("%d",&ui);
+	while(line<=ui) {
 		i=0;	
-	while(i<(ui-line))
-	{
-		printf(" ");
-i++;
-
-}
-
-	j=0;
-		while(j<((2*line)-1))
-{
-	printf("*");
-j++;
-}
-
-
-printf("\n");
-line++;
-}
-
+		while(i<(ui-line)) {
+			printf(" ");
+			i++;
+		}
+		j=0;
+		while(j<((2*line)-1)) {
+			printf("*");
+			j++;
+		}
+		printf("\n");
+		line++;
+	}
+	return 0;
 }
 
