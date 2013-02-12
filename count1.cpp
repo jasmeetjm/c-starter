d939487f0c34cccd6f5614950d7bd1ba418ac06a
jasmeet <jasmeetjm@gmail.com>
2013-02-12 23:58:35 +0530

    Update count1.cpp

--- count1.cpp
+++ count1.cpp
@@ -1,20 +1,20 @@
+/*to count no. of 1's in bits representation of an integer */
+
 #include<iostream>
 using namespace std;
-int count=0;
-int count1(int num)
-{
+  int count1(int num) {
+  int count=0;        //local variable used to count no. of ones
+  while (num!=0)  {
+    num=num&num-1;  //to count no. of ones till all are counted
+    count++;
+  }
+  return count;
+}
 
-while (num!=0) 
-{
-num=num&num-1;
-count++;
-}
-return count;
-}
 int main()
 {
-int num;
-cin>>num;
-cout<<"the number of 1's are"<<count1(num)<<endl;
-return 0;
+  int num;  //variable
+  cin>>num; //user input
+  cout<<"the number of 1's are"<<count1(num)<<endl;
+  return 0;
 }
