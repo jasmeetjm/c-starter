625abbdc995a085146531417ea161a315a5e4a15
jasmeet <jasmeetjm@gmail.com>
2013-02-13 00:45:25 +0530

    Update file_wordcount.c

--- file_wordcount.c
+++ file_wordcount.c
@@ -1,33 +1,31 @@
+/*to count no. of words in a file */
+
+
 #include<stdlib.h>
 #include<stdio.h>
 int main()
-{		FILE *fptr,*fptr1;
-		char filename[]="2.txt";
-		fptr=fopen(filename,"r");
-		char ch;
-		fptr1=fopen(filename,"r");
-		char ch1=' ';
-		int count=0;
-			
-		if(fptr==NULL)
 {
+	FILE *fptr,*fptr1; //creating two file pointers
+	char filename[]="2.txt";
+	fptr=fopen(filename,"r");
+	char ch;
+	fptr1=fopen(filename,"r");
+	char ch1=' ';
+	int count=0;
+	if(fptr==NULL) {	//if file not opened
 		fprintf(stderr,"NOTHING");
 		exit(-1);
-}
-			
-		while((ch=getc(fptr))!=EOF)
-{
-		if(ch == ' '|| ch == '\t' || ch == 10)
-{
-	if((ch1  >= 'a' && ch1 <='z') || (ch1 >= 'A' && ch1 <= 'Z') || (ch1 >='0' && ch1 <= '9'))
-{
-	printf("jasmeet\n");
-	count++;
-}
-}
-ch1=ch;
-
-}
-	fclose(fptr);
+	}
+	while((ch=getc(fptr))!=EOF) {	//fetching char by char till eof
+		if(ch == ' '|| ch == '\t' || ch == 10) { // when word finishes
+			if((ch1  >= 'a' && ch1 <='z') || (ch1 >= 'A' && ch1 <= 'Z') || (ch1 >='0' && ch1 <= '9')) { //to check if a given entry is a word or not
+				printf("jasmeet\n");
+				count++;
+			}
+		}
+		ch1=ch;
+	}
+	fclose(fptr);	//closing a file
 	fprintf(stdout,"%d",count);
+	return 0;
 }
