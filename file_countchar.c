acc314c5ecaf24092f08a85d8c5998acbdc15d52
jasmeet <jasmeetjm@gmail.com>
2013-02-13 00:33:50 +0530

    Update file_countchar.c

--- file_countchar.c
+++ file_countchar.c
@@ -1,26 +1,25 @@
+/* to count the no. of characters in a file */
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-int count=0;
-char ch;
-FILE *fptr;
-char filename[]="k.txt";
-fptr=fopen(filename,"r");
-if(fptr==NULL)
-{
-fprintf(stderr,"NOTHING");
-exit(-1);
-}
-while ((ch=getc(fptr))!=EOF)
-{
-printf("%c",ch);
-count++; 
-fprintf(stdout,"%c",ch);
-}
-count--;
-fprintf(stdout,"%d",count);
-fclose(fptr);
-return 0;
-
+  int count=0;  //variable to count no. of characters
+  char ch;      //variable to store char  
+  FILE *fptr;   //file pointer
+  char filename[]="k.txt";  // filename declare
+  fptr=fopen(filename,"r"); //opening the file
+  if(fptr==NULL) {          //if file is not opened
+    fprintf(stderr,"NOTHING");
+    exit(-1);
+  }
+  while ((ch=getc(fptr))!=EOF) {  //till end of file is achieved
+    printf("%c",ch);              // print every character by character till eof
+    count++;                      //counting no. of characters
+    fprintf(stdout,"%c",ch);
+  }
+  count--;                        
+  fprintf(stdout,"%d",count);
+  fclose(fptr);
+  return 0;
 }
