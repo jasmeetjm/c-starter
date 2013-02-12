b6e86f5aa406cf0ae7469b75671e3a7afe8bae49
jasmeet <jasmeetjm@gmail.com>
2013-02-13 00:28:03 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,19 +1,17 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-FILE *fptr,*fptr1;
-int a=7,b=8;
-
-char filename[]="2.txt";
-fptr=fopen(filename,"w");
-char ch;
-
-if(fptr==NULL)
-{
-	printf("error");
-	exit(-1);
-}
+	FILE *fptr,*fptr1;
+	int a=7,b=8;
+	char filename[]="2.txt";
+	fptr=fopen(filename,"w");
+	char ch;
+	if(fptr==NULL) {
+		fprintf(stderr,"error");
+		exit(-1);
+	}
 	fprintf(stdout,"hello jasmeet\n");
 	fprintf(fptr,"hello world\n");
 	fprintf(fptr,"a:->%d b:->%d\n",a,b);
@@ -22,24 +20,18 @@ if(fptr==NULL)
 	fscanf(stdin,"%d%d",&a,&b);
 	fprintf(fptr,"%d\t%d\n",a,b);
 	fprintf(stderr,"jasmeet has put an error\n");
-
-fclose(fptr);
-
-fptr=fopen(filename,"r");
-while((ch=getc(fptr))!=EOF)
-{
-printf("%c",ch);
-
-}
-fclose(fptr);
-fptr=fopen(filename,"r");
-fptr1=fopen("1.txt","a");
-while((ch=getc(fptr))!=EOF)
-{
-fprintf(fptr1,"%c",ch);
-}
-fclose(fptr);
-fclose(fptr1);
-return 0;
-
+	fclose(fptr);
+	fptr=fopen(filename,"r");
+	while((ch=getc(fptr))!=EOF) {
+		printf("%c",ch);
 	}
+	fclose(fptr);
+	fptr=fopen(filename,"r");
+	fptr1=fopen("1.txt","a");
+	while((ch=getc(fptr))!=EOF)
+		fprintf(fptr1,"%c",ch);
+	
+	fclose(fptr);
+	fclose(fptr1);
+	return 0;
+}
