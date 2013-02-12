ab9ab23ed53720875e187c73c65e0de6f31e131c
jasmeet <jasmeetjm@gmail.com>
2013-02-12 23:30:40 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,66 +1,30 @@
-#include<stdio.h>
+/*to find given no. is armstrong or not*/
 
+#include<stdio.h>
 #include<math.h>
 int main()
 {
-
-
-
-int i;
-int counter=0;
-for(i=0;i<100000000;i++)
-{
-//scanf("%d",&i);
-int count=0;
- int temp=i;
+ int i;  //variable for an input
+ scanf("%d",&i); //user input
+ int count=0; //to count no. of digits
+ int temp=i; // temp is variable where we store given input
 /*while(temp!=0)
 {
 temp=temp/10;
 count++;
 }
 */
-
-//count=printf("%d",temp);
-count = log(temp)+1;
-
-int sum=0;
-while(temp!=0)
-{
-
-sum=sum + pow((temp%10),count);
-temp=temp/10;
-}
-
-/*if(sum == i)
-{
-printf("%d is an armstrong no\n",i);
-//scounter++;
-
-}
-else
-{
-printf("");
-//printf("%d is not an armstrong number\n",i);
-}*/
-do
-{printf("%d is an armstrong number",i);
-}
-while(sum==i);
-/*if(sum ==i)
-{
-counter++;
-}
-
-else
-{
-bounter ++;
-}
-}
-printf("NO. OF ARMSTRONG NUMBER IN 0-100000 IS %d",counter);
-printf("NO. OF non ARMSTRONG NUMBER IN 0-100000 IS %d",bounter);
-*/
-//}
-//printf("\t %d no. of armstrong no.",counter);
-
-}
+ count=printf("%d",temp); //count no. of digits in a given input
+//        count = log(temp) + 1;
+ int sum=0;    //sum variable to check if a given no. gives desired result
+ while(temp!=0) {
+  sum=sum + pow((temp%10),count); //formula to check 
+  temp=temp/10;
+ }
+ if(sum == i) 
+  printf("%d is an armstrong no\n",i);
+ else
+  printf("%d is not an armstrong number\n",i);
+  }
+  return 0;
 }
