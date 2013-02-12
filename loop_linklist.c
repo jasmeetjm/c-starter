6d5f97738e4cd43186f165748fee219f13a5bced
jasmeet <jasmeetjm@gmail.com>
2013-02-13 01:24:20 +0530

    Update loop_linklist.c

--- loop_linklist.c
+++ loop_linklist.c
@@ -1,110 +1,103 @@
+/*to detect if there is a loop in a link list and correct it */
+
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct link_list
+typedef struct link_list //creating a structure for a link list
 {
-	int data;
+	int data;		
 	struct link_list *next;
-}ll;
-	ll *head=NULL;
-	ll *temporary=NULL;
-ll insert(int num)
+}list;
+
+list *head=NULL;
+list *temporary=NULL;
+
+list insert(int num)		//to insert node in a link list
 {
-	if(head==NULL)
-{
-	head=malloc(sizeof(ll));
+	if(head==NULL) {
+	head=malloc(sizeof(list));
 	head->data=num;
 	head->next=NULL;
 	return *head;
-}
-	ll *temp;
- temp=malloc(sizeof(ll));
-	
+	}
+	list *temp;
+ 	temp=malloc(sizeof(list));
 	temp=head;
 	while(temp->next!=NULL)
-{
+		temp=temp->next;
 
-	temp=temp->next;
-
-}
-	temp->next=malloc(sizeof(ll));
+	temp->next=malloc(sizeof(list));
 	temp->next->data=num;
 	temp->next->next=NULL;
 	temp=temp->next;
 	return *temp;
-
 }
-	ll enter_loop(int num)
+
+list enter_loop(int num)		//to enter into a loop manually
 {
-	ll *temp=malloc(sizeof(ll));
+	list *temp=malloc(sizeof(list));
 	temp=head;
 	while(temp->data!=num)
-{
-	temp=temp->next;
-}
-	ll *temp1=malloc(sizeof(ll));
+		temp=temp->next;
+
+	list *temp1=malloc(sizeof(list));
 	temp1=temp;
 	while(temp1->next!=NULL)
-{
-	temp1=temp1->next;
-}
+		temp1=temp1->next;
+
 	temp1->next=temp;
 	printf("%d\n",temp1->next->data);
 	return *temp1;	
 }
-	ll loop_detection()
+
+list loop_detection()		//to detect a loop
 {
-	ll *temp1=malloc(sizeof(ll));
-	temp1=head;
-	ll *temp2=malloc(sizeof(ll));
+	list *temp1=malloc(sizeof(list));	//declare 1st pointer	
+	temp1=head;		
+	list *temp2=malloc(sizeof(list));	//declare 2nd pointer
 	temp2=head;
 	printf("%d\n",temp2->next->data);	
-while(temp2->next!=NULL && temp2->next->next!=NULL)
-{
-	temp1=temp1->next;
-	temp2=temp2->next->next;
-	printf("%d\t%d\n",temp1->data,temp2->data);	
-if(temp1==temp2)
-{
-	printf("there is loop\n");
-	temporary=malloc(sizeof(ll));
-	temporary=temp1;
-	return *temp1;
-}		
-}		
-
+	while(temp2->next!=NULL && temp2->next->next!=NULL) {	//increase one pointer by 1 and another by 2.
+		temp1=temp1->next;
+		temp2=temp2->next->next;
+		printf("%d\t%d\n",temp1->data,temp2->data);	
+		if(temp1==temp2) {			//if they meet at any point,it proves there is a loop
+			printf("there is loop\n");
+			temporary=malloc(sizeof(list));
+			temporary=temp1;
+			return *temp1;
+		}		
+	}			
 	printf("there is no loop\n");		
 	return ;	
 }
-	ll loop_correction()
+
+list loop_correction()			//to correct a loop
 {
-	if(temporary==NULL)
-{
-	printf("there is no loop\n");
-	return;
-}
-	ll *temp=malloc(sizeof(ll));
+	if(temporary==NULL) {
+		printf("there is no loop\n");
+		return;
+	}
+	list *temp=malloc(sizeof(list));		
 	temp=head;
-while(temporary->next->data!=temp->next->data)
-{
-	temporary=temporary->next;
-	temp=temp->next;
-	printf("%d\t%d\n",temporary->data,temp->data);
-}	
+	while(temporary->next->data!=temp->next->data) {//increase pointer 1 from head & another where they met in loop_detection one by one
+		temporary=temporary->next;		
+		temp=temp->next;	//they will meet at the point where there is looppoint
+		printf("%d\t%d\n",temporary->data,temp->data);
+	}	
 	printf("%d\n",temp->next->data);
-	temporary->next=NULL;
+	temporary->next=NULL;		//removing loop
 	return *(temp->next);
 }
 
 int main()
 {
 	int i=0;
-	while((4*i)<100)
-{
+	while((4*i)<100) {
 		insert((4*i));
 		++i;
 	}
-		enter_loop(44);
+	enter_loop(44);
 /*		while(head->next!=NULL)
 {
 	printf("%d\t",head->data);
@@ -113,10 +106,9 @@ int main()
 	loop_detection();
 	loop_correction();
 
-		while(head->next!=NULL)
-{
-	printf("%d\t",head->data);
-	head=head->next;
-}
-		return 0;
+	while(head->next!=NULL) {
+		printf("%d\t",head->data);
+		head=head->next;
+	}
+	return 0;
 }
