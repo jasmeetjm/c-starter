3a75e7b4a0710c95af2fb526311d06ed485ce8d3
jasmeet <jasmeetjm@gmail.com>
2013-02-12 22:55:09 +0530

    Update and_count1.c

--- and_count1.c
+++ and_count1.c
@@ -1,16 +1,16 @@
+/* to Count no. of one's in the given integer in bits representation. */
+
 #include<stdio.h>
 int main()
 {
-int num1;
-int count1=0;
-printf("enter number");
-scanf("%d",&num1);
-while (num1!=0)
-{
-num1=num1 & (num1 - 1);
-count1++;
-}
-printf("%d",count1++);
-return 0;
-
+  int num1,     //variable to take integer
+      count1=0; //variable to count no. of one's
+  printf("enter number");
+  scanf("%d",&num1);  //take input from user
+  while (num1!=0) {   //till all one's are counted
+    num1=num1 & (num1 - 1); // this reduce no. of one's in a number one less than before
+    count1++;               //counting everytime it works
+  }
+  fprintf(stdout,"%d",count1++);  //tells the output
+  return 0;
 }
